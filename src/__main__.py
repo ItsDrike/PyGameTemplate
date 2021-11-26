5bfe1d52ab8e9f11b5fd6757100f17e4f8db33c7
ItsDrike <itsdrike@protonmail.com>
2021-11-26 17:39:55 +0100

    Add __main__ script to start the game

---
+++ src/__main__.py
@@ -0,0 +1,18 @@
+from contextlib import suppress
+
+from src.config import Window
+from src.game import Game
+from src.util.log import get_logger
+
+log = get_logger()
+
+
+if __name__ == "__main__":
+    game = Game(Window.width, Window.height, Window.tick_rate)
+
+    log.info("Starting game")
+
+    with suppress(KeyboardInterrupt):
+        game.run_continually()
+
+    log.info("Game Stopped")
