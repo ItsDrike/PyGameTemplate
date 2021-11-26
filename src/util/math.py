aa966256162494e83e02c22ec5944dd63e9755ea
ItsDrike <itsdrike@protonmail.com>
2021-11-26 18:05:38 +0100

    Fix docstring example

--- src/util/math.py
+++ src/util/math.py
@@ -14,11 +14,11 @@ def number_remap(
     For example, remapping a number ranging between 0 and 10 to new range of 0 and 100
     would result in following:
         >>> number_remap(1, 0, 10, 0, 100)
-        >>> 10
+        10
         >>> number_remap(2, 0, 10, 0, 100)
-        >>> 20
+        20
         >>> number_remap(10, 0, 10, 0, 100)
-        >>> 100
+        100
     """
     if number > old_max or number < old_min:
         raise ValueError(f"Specified number: {number} is not within specified old range: <{old_min}, {old_max}>")
