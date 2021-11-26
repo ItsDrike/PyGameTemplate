21d01e3209f1ab74632ba3dbc1e856d1c59af7bc
ItsDrike <itsdrike@protonmail.com>
2021-11-26 22:28:14 +0100

    Add RadianAngle descriptor

--- src/util/descriptor.py
+++ src/util/descriptor.py
@@ -1,4 +1,5 @@
-from typing import Any, Generic, TypeVar, cast
+import math
+from typing import Any, Generic, TypeVar
 
 from src.util.typing import NumericType
 
@@ -62,36 +63,107 @@ class Descriptor(Generic[T]):
         del instance.__dict__[self.name]
 
 
-class Coordinate(Descriptor[T]):
+class TypedDescriptor(Descriptor[T]):
+    """
+    Decorator which only accepts values of given type(s).
+
+    This class is expected to be subclassed and override the class variables
+    `allowed_types`, holding the tuple of accepted types, for isinstance checks.
+    """
+
+    allowed_types = (object, )
+
+    def check(self, value: Any) -> None:
+        """Ensure given `value` meets the requirements."""
+        if not isinstance(value, self.allowed_types):
+            if len(self.allowed_types) == 1:
+                types_msg = f"must be of type: `{self.allowed_types[0]}`"
+            else:
+                joined = ", ".join(f"`{typ}`" for typ in self.allowed_types)
+                types_msg = f"type must be one of: {joined}"
+
+            raise TypeError(f"{self.name} {types_msg}, got `{value.__class__.__qualname__}`")
+
+    def __set__(self, instance: T, value: Any) -> None:
+        """Only allow setting values which mach the expected types."""
+        self.check(value)
+        return super().__set__(instance, value)
+
+
+class Numeric(TypedDescriptor[T]):
+    """
+    Any number (int/float).
+
+    The setter will raise an exception if the user will try to set the parameter to a
+    non-numeric value (TypeError).
+    """
+
+    allowed_types = (float, int)
+    ValueType = NumericType
+
+
+class NumericRange(Numeric[T]):
+    """
+    Any number within specified range.
+
+    Range is received with init using `min` and `max` keyword arguments, both `min`
+    and `max` are included in this range.
+
+    The setter will raise an exception if user will try to set the parameter to a:
+        * non-numeric value -> `TypeError`
+        * value outside of allowed range -> `ValueError`
+    """
+
+    def __init__(self, *args, min: NumericType, max: NumericType, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.min = min
+        self.max = max
+
+    def check(self, value: Any) -> None:
+        """Ensure given `value` meets the requirements."""
+        super().check(value)
+        if value < self.min or value > self.max:
+            raise ValueError(f"Given value ({value}) is outside the allowed range <{self.min}-{self.max}>.")
+
+
+class RadianAngle(NumericRange[T]):
+    """
+    Angle represented as a number in radians.
+
+    If a number is outside of the allowed range, it is automatically
+    adjusted (value % 2 * pi) to always represent a valid basic radian angle.
+
+    The setter will raise an exception if user will try to set the parameter to
+    a non-numeric value (TypeError).
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, min=0, max=(2 * math.pi), **kwargs)
+
+    def __set__(self, instance: T, value: Any) -> None:
+        """Automatically adjust value to fit within the base radian range."""
+        # We don't adjust the number first because it may not even be of
+        # numeric type, running the checks first ensures that it is numeric
+        # and will fail with ValueError only if it's outside of specified range.
+        try:
+            self.check(value)
+        except ValueError:
+            value = value % 2 * math.pi
+        return super().__set__(instance, value)
+
+
+class Coordinate(NumericRange[T]):
     """
     Coordinate parameter descriptor.
 
     This imposes window border bound restrictions automatically.
     Window size is received with init using `window_border` keyword argument.
+
     The setter will raise an exception if user will try to set the parameter to a:
-        * non-float value -> `TypeError`
+        * non-numeric value -> `TypeError`
         * value outside of allowed bounds -> `ValueError`
     """
 
     def __init__(self, *args, window_border: NumericType, **kwargs):
         self.window_border = window_border
-
-    def __get__(self, instance: T, owner_cls: type[T]) -> float:
-        """Implement Coordinate getter."""
-        ret = super().__get__(instance, owner_cls)
-        return cast(float, ret)  # This will be float, since it's imposed in setter
-
-    def __set__(self, instance: T, value: NumericType) -> None:
-        """
-        Implemnet Coordinate setter.
-
-        - If the value isn't int/float, raise `TypeError`
-        - If the value is out of given bounds, raise `ValueError`
-        """
-        if not isinstance(value, (int, float)):
-            raise TypeError(f"{self.name} coordinate must be `int` or `float`, got {value.__class__.__qualname__}")
-
-        if value > self.window_border or value < 0:
-            raise ValueError(f"Out of bounds: {self.window_border} can't be outside of the window (0-{self.window_border}), got {value}")
-
-        return super().__set__(instance, float(value))
+        super().__init__(*args, min=0, max=self.window_border, **kwargs)
